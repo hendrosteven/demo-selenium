5e47230321ccda88134b11ce5b0cca5dcbeb1444
Hendro Steven <hendro.steven@gmail.com>
2021-04-08 13:32:06 +0700

    Screenshot Update with custome file name Test

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,6 +3,7 @@ package base;
 import com.google.common.io.Files;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -11,6 +12,8 @@ import utility.WindowManager;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -39,10 +42,19 @@ public class BaseTest {
     }
 
     @AfterMethod
-    public void takeScreenshot() throws IOException {
-        TakesScreenshot camera = (TakesScreenshot) driver;
-        File screenshot = camera.getScreenshotAs(OutputType.FILE);
-        Files.move(screenshot, new File("screenshot/test.png"));
+    public void takeScreenshot(ITestResult result) throws IOException {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            TakesScreenshot camera = (TakesScreenshot) driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            Files.move(screenshot,
+                    new File("screenshot/" + currentDateTime()
+                            + "_" + result.getName() + ".png"));
+        }
+    }
+
+    private String currentDateTime(){
+        SimpleDateFormat sdm = new SimpleDateFormat("yyyy-dd-MM HH:mm:ss");
+        return sdm.format(new Date());
     }
 
 }
